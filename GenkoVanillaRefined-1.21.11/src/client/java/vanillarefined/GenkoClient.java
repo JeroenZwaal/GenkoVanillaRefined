48485cdb6133492cc18d778c0b99d09d3aac6187
Jeroen Zwaal <93530512+JeroenZwaal@users.noreply.github.com>
2026-01-18 23:41:20 +0100

    added the template

---
+++ GenkoVanillaRefined-1.21.11/src/client/java/vanillarefined/GenkoClient.java
@@ -0,0 +1,10 @@
+package vanillarefined;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GenkoClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
